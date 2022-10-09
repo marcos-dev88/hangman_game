44f31a3182cc4142d05db3c4091ff355ed742adb
marcos-dev88 <mv.piesske@gmail.com>
2022-10-09 12:08:22 -0300

    improving function declaration
    Signed-off-by: marcos-dev88 <mv.piesske@gmail.com>

---
+++ headers_hang.h
@@ -0,0 +1,9 @@
+// all function declaration
+int printArt(int tryLost, int bodyParts[7]);
+void generateGround(char ground[], char const *secretWord);
+void checkMissed(int *isRight, int *miss);
+void lostGameScreen();
+void winScreen();
+int checkWon(int hit, char const *secretWord);
+int fileLines(const char *restrict);
+
