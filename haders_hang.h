c8c807939bd65a6ca30172fcbea39928c9797fee
marcos-dev88 <mv.piesske@gmail.com>
2022-10-09 11:25:17 -0300

    creating new function implementation to win
    Signed-off-by: marcos-dev88 <mv.piesske@gmail.com>

--- haders_hang.h
+++ haders_hang.h
@@ -2,8 +2,9 @@
 int printArt(int tryLost, int bodyParts[7]);
 void generateGround(char ground[], char const *secretWord);
 void checkMissed(int *isRight, int *miss);
-void chooseSecretWord(char const *secretWord);
+void chooseSecretWord(char *secretWord);
 void lostGameScreen();
-void checkWon(int *point, int hit, char const *secretWord);
+void winScreen();
+int checkWon(int hit, char const *secretWord);
 int fileLines(const char *restrict);
 
